e91969570cd41f414bc43a08b9f7bb16d194ada2
teraJumper98193 <tomasz.swider@gmail.com>
2016-12-27 20:33:17 -0500

    Assignment 25 completed

--- album.js
+++ album.js
@@ -30,6 +30,20 @@ var albumMarconi = {
     ]
 };
 
+var albumSwider = {
+   title: 'The Speed',
+   artist: 'Tomasz Swider',
+   label: "MGM",
+   year: '2016'
+   albumArtUrl: 'assets/images/album_covers/me.png'
+   songs: [
+     { title: 'Hello, Is this On?', duration: '2:10'},
+     { title: 'Anybody hear me?', duration: '3:03'},
+     { title: 'Wish you were here', duration: '4:04'},
+     { title: 'Over and Out', duration: '5:05'}
+  ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -42,13 +56,13 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
     // #2
     albumTitle.firstChild.nodeValue = album.title;
@@ -67,4 +81,14 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
-};
+
+    var albums = [albumPicasso, albumMarconi, albumSwider];
+    var index = 1;
+    albumImage.addEventListener("click", function(event) {
+      setCurrentAlbum(albums[index]);
+      index++;
+      if (index==albums.length) {
+        index=0
+      }
+    )};
+  };
